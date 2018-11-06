a964d9a332e16b3d89244f61fd7c34a4150cf04b
hiroshi seki <seki-h@klab.com>
2018-11-06 17:01:55 +0900

    py3 implement

--- rlogger/handlers.py
+++ rlogger/handlers.py
@@ -1,27 +1,23 @@
 import logging.handlers
 import socket
-import sys
 import struct
+import sys
 import time
 
 
-class rloggerNativeHandler(logging.handlers.SocketHandler):
+class RLoggerNativeHandler(logging.handlers.SocketHandler):
     HDR_VERSION = 1
     HDR_TYPE_PSH = 1
     HDR_PACK_FMT = '!BBHLL'
     SBUF_PACK_FMT = '!LL'
 
-    def __init__(self,
-                 tag,
-                 host='', port=10382, socket_path=None,
-                 chunk_size=8388608):
+    def __init__(self, tag, host='', port=10382, socket_path=None, chunk_size=8388608):
         logging.handlers.SocketHandler.__init__(self, host, port)
-        if not isinstance(tag, basestring):
+        if not isinstance(tag, str):
             raise TypeError('tag is not string')
         self.socket_path = socket_path
         self.tag = tag.encode(sys.getdefaultencoding())
-        self.hdr_size = len(
-            struct.pack(rloggerNativeHandler.HDR_PACK_FMT, 0, 0, 0, 0, 0))
+        self.hdr_size = len(struct.pack(RLoggerNativeHandler.HDR_PACK_FMT, 0, 0, 0, 0, 0))
         self.sbuf_size = chunk_size - (self.hdr_size + len(self.tag))
 
     def make_unix_socket(self):
@@ -36,7 +32,7 @@ class rloggerNativeHandler(logging.handlers.SocketHandler):
             if self.retryTime is None:
                 attempt = 1
             else:
-                attempt = (now >= self.retryTime)
+                attempt = now >= self.retryTime
             if attempt:
                 try:
                     self.sock = self.make_unix_socket()
@@ -45,15 +41,13 @@ class rloggerNativeHandler(logging.handlers.SocketHandler):
                     if self.retryTime is None:
                         self.retryPeriod = self.retryStart
                     else:
-                        self.retryPeriod = self.retryPeriod * self.retryFactor
-                        if self.retryPeriod > self.retryMax:
-                                self.retryPeriod = self.retryMax
+                        self.retryPeriod = min(self.retryPeriod * self.retryFactor, self.retryMax)
                     self.retryTime = now + self.retryPeriod
         else:
             logging.handlers.SocketHandler.createSocket(self)
 
     def sbuf_gen(self, t, s):
-        b = buffer(s.encode(sys.getdefaultencoding()))
+        b = memoryview(s.encode(sys.getdefaultencoding()))
         sp = 0
         while True:
             ep = sp + self.sbuf_size
@@ -61,27 +55,24 @@ class rloggerNativeHandler(logging.handlers.SocketHandler):
             n = len(v)
             if n == 0:
                 break
-            yield struct.pack(
-                rloggerNativeHandler.SBUF_PACK_FMT + str(n) + 's',
-                int(t), n, v
-            )
+            yield struct.pack(RLoggerNativeHandler.SBUF_PACK_FMT + str(n) + 's', int(t), n, v.tobytes())
             sp += n
 
     def make_single_packet(self, buf):
         s = struct.pack(
-            rloggerNativeHandler.HDR_PACK_FMT,
-            rloggerNativeHandler.HDR_VERSION,
-            rloggerNativeHandler.HDR_TYPE_PSH,
+            RLoggerNativeHandler.HDR_PACK_FMT,
+            RLoggerNativeHandler.HDR_VERSION,
+            RLoggerNativeHandler.HDR_TYPE_PSH,
             self.hdr_size + len(self.tag),
             0,
-            self.hdr_size + len(self.tag) + len(buf)
+            self.hdr_size + len(self.tag) + len(buf),
         )
         s += self.tag + buf
         return s
 
     def packet_gen(self, r):
         t = time.time()
-        b = ''
+        b = b''
         for ln in self.format(r).splitlines():
             for s in self.sbuf_gen(t, ln):
                 if self.sbuf_size < len(b) + len(s):
@@ -97,23 +88,23 @@ class rloggerNativeHandler(logging.handlers.SocketHandler):
                 self.send(p)
         except (KeyboardInterrupt, SystemExit):
             raise
-        except:
+        except Exception as e:
             self.handleError(record)
 
+
 if __name__ == '__main__':
+
     def main():
         logger = logging.getLogger('')
         logger.setLevel(logging.DEBUG)
-        rh = rloggerNativeHandler(
-            'myproject.sample.myhost',
-            socket_path='/var/run/rloggerd/myproject.sock'
-        )
-#        rh = rloggerNativeHandler(
-#            'myproject.sample.myhost',
-#            host='localhost', port=5555
-#        )
+        rh = RLoggerNativeHandler('myproject.sample.myhost', socket_path='/var/run/rloggerd/myproject.sock')
+        #        rh = rloggerNativeHandler(
+        #            'myproject.sample.myhost',
+        #            host='localhost', port=5555
+        #        )
         logger.addHandler(rh)
         logger.warning("python rlogger sample 1\npython rlogger sample 2\n")
         time.sleep(60)
         logger.warning("python rlogger sample 3\npython rlogger sample 4\n")
+
     main()
